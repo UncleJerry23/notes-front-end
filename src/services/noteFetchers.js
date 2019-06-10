d1eae2f5ae2b8ac1ea9896c3f0575d2660ae2674
UncleJerry <jared.myhrberg@gmail.com>
2019-06-10 14:05:57 -0700

    changed services

--- src/services/noteFetchers.js
+++ src/services/noteFetchers.js
@@ -1,7 +1,26 @@
 export const createNote = note => {
-  return Promise.all([
-    { ...note,
-      _id: '1234567890' 
-    }
-  ]);
+  return fetch('https://git.heroku.com/notes-for-dayz.git', {
+    method: 'POST',
+    body: JSON.stringify(note),
+    headers:{
+      'Content-Type': 'application/json'
+    } })
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Unable to fetch';
+      return json;
+    });
 };
+
+export const getAllNotes = () => {
+  return fetch('https://git.heroku.com/notes-for-dayz.git', {
+    method: 'GET',
+    headers:{
+      'Content-Type': 'application/json'
+    } })
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Unable to fetch';
+      return json;
+    });
+}
