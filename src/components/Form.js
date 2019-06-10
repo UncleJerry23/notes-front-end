c08670427c507ae160545c293f61fe1a771e9179
UncleJerry <jared.myhrberg@gmail.com>
2019-06-10 10:51:37 -0700

    styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function NoteDForm({ title, body, handleSubmit, handleChange }) {
+  const formStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'start'
+  };
+
+  const buttonStyle = {
+    width: '80px',
+    margin: '10px'
+  };
+
+  const titleInput = {
+    width: '225px',
+    textAlign: 'center',
+    margin: '10px'
+  };
+
+  const bodyInput = {
+    height: '200px',
+    width: '400px',
+    margin: '10px'
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <button>Submit</button>
-      <input type="text" name="title" value={title} placeholder={title || 'Note Title'} onChange={handleChange} />
-      <input type="textfield" name="body" value={body} placeholder={body || 'Note Body'} onChange={handleChange} />
+    <form onSubmit={handleSubmit} style={formStyle}>
+      <button style={buttonStyle}>Submit</button>
+      <input style={titleInput} type="text" name="title" value={title} placeholder={title || 'Note Title'} onChange={handleChange} />
+      <textarea style={bodyInput} name="body" value={body} placeholder={body || 'Note Body'} onChange={handleChange} />
     </form>
   );
 
